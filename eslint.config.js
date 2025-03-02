d657d5bbc9477a66d6dae0293a40b933525b25eb
mario <Mamm1201@users.noreply.github.com>
2025-03-02 14:14:31 -0500

    Configuración de ESLint completada

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,18 @@
-// eslint.config.js
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    ignores: ["node_modules", "dist"],
-  },
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
 ];
