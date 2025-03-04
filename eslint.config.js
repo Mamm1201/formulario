11f524ec734d391deae921118fcccd4ff716374e
mario <Mamm1201@users.noreply.github.com>
2025-03-04 15:45:06 -0500

    Modificaciones de sistaxis en archivos eslint.config.js y server.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,18 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Para frontend
+        ...globals.node, // Agregar soporte para Node.js
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module", // Si usas import/export en lugar de require
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = requiere("express");
+const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join("dist,", "index.html")));
+// Serve static files
+app.use(express.static(path.join(__dirname, "dist,")));
 
+// for any request that doesn't match a static file, serve, index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(path.dirname, "dist", "index.html)"));
+  res.sendFile(path.join(__dirname, "dist", "index.html)"));
 });
 
-const port = process.env.PORT || 8000;
+// Expose
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  return `server runinig on prot:${PORT}`;
+  console.log(`server runinig on prot:${PORT}`);
 });
